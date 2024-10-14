661e45e667cbc019b54b958471874852b11885c6
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-10-14 09:06:29 +0200

    Add files via upload

---
+++ Lokale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECForedrag
+{
+ 
+
+
+     class Lokale
+    {
+        public string Navn { get; set; }
+
+
+        public Lokale(string navn)
+        {
+            Navn = navn;    
+        }
+
+    }
+}
