0c7ffab2dab6353dce146f49ecf2f250a897e146
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-10-15 07:39:00 +0200

    Add files via upload

---
+++ Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECForedrag
+{
+    class Person
+    {
+       public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public Person(string navn, int alder)
+        {
+            Navn = navn;
+            Alder = alder;
+        }
+
+
+
+
+      
+
+    }
+}
