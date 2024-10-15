1e6ab336b6bc233a69e3de362380957c10bd1970
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-10-15 09:14:06 +0200

    Update Person.cs

--- Models/Person.cs
+++ Models/Person.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TEC-Lecture-System
+namespace TECLectureSystem.Models
 {
     class Person
     {
